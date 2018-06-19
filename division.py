c8d3206f7e322ad44a5af84f79a719a8c297d820
Karina Kozarova <karinakozarova1303@gmail.com>
2018-06-19 22:49:24 +0300

    Create division.py

---
+++ division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # Read two integers and
+    a = int(input())
+    b = int(input())
+    
+    # The first line should contain integer division, a//b . 
+    print(a//b)
+    # The second line should contain float division, a/b . 
+    print(a/b)
