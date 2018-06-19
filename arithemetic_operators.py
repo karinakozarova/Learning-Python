c21f808131015eb8b3f7e4d33668a810b5a83ffb
Karina Kozarova <karinakozarova1303@gmail.com>
2018-06-19 22:47:22 +0300

    Create arithemetic_operators.py

---
+++ arithemetic_operators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+  # Read two integers from STDIN
+    a = int(input())
+    b = int(input())
+
+    print(a+b) # the sum of the two numbers
+    print(a-b) # the difference of the two numbers (first - second).
+    print(a*b) # the product of the two numbers.
