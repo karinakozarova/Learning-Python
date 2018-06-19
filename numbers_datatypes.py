3976b0a8dc573942eaf5ba92860d9a6cc0c529e6
Karina Kozarova <karinakozarova1303@gmail.com>
2018-06-19 23:20:38 +0300

    Update numbers_datatypes.py

--- numbers_datatypes.py
+++ numbers_datatypes.py
@@ -1,3 +1,11 @@
 cucumbers = 2
 price_per_cucumber = 3.25
 total_cost = cucumbers * price_per_cucumber
+
+cucumbers = 100
+num_people = 6
+whole_cucumbers_per_person = cucumbers / num_people
+print whole_cucumbers_per_person
+
+float_cucumbers_per_person = cucumbers*1.0 / num_people
+print float_cucumbers_per_person
