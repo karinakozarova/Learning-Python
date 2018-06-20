e2cef4e6162f580840d2af698e857094d35e8039
karinakozarova <karinakozarova1303@gmail.com>
2018-06-20 18:28:48 +0300

    more hackerrank

---
+++ int_sizes.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(str(pow(a,b)+(pow(c,d))))
---
+++ power.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(str(pow(a,b)))
+print(str(pow(a,b,c)))
