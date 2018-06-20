59f763098933fed1858c3c39817fd6d007da97fa
karinakozarova <karinakozarova1303@gmail.com>
2018-06-20 18:34:51 +0300

    hackerranking

---
+++ test.py
@@ -0,0 +1,13 @@
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+   for x in range(1,i): 
+      print(i-1,end='') 
+      
+   print('')
+
+
+
+
+
+
+
+   
