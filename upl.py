79e207967d2f6dcadf5c44256347717e03278756
karinakozarova <karinakozarova1303@gmail.com>
2018-06-22 00:13:47 +0300

    added more python exercises

--- upl.py
+++ upl.py
@@ -4,4 +4,6 @@ import sys
 
 if __name__ == "__main__":
     print sys.argv[1]
+    for i in range(1,len(sys.argv)):
+        print sys.argv[i]
     os.system('git add . && git commit -m "added more python exercises" && git push') 
