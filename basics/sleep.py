ca9f8fcab22d024816ea7629183b90612a9ed5fe
karinakozarova <karinakozarova1303@gmail.com>
2018-06-24 10:53:02 +0300

    added midnight code for sleeping

---
+++ basics/sleep.py
@@ -0,0 +1,17 @@
+def commit_to_github():
+    print("COMMITED!!! PUSHED to master branch..")
+
+def eat():
+    print("Ate Slatinska pizza...")
+
+def sleep():
+    graduated = False
+    if not graduated:
+        print("Ugh, Kiko Mitov is waiting :( ")
+    else: 
+        print("Sleep like a baby")  
+
+if __name__ == '__main__':
+    commit_to_github()
+    eat()
+    sleep()
