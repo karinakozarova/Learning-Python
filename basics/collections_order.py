df079731a18fd7b81d6524d86ec705cf641ce515
karinakozarova <karinakozarova1303@gmail.com>
2018-06-25 01:06:12 +0300

    optimized solution

--- basics/collections_order.py
+++ basics/collections_order.py
@@ -1,14 +1,11 @@
 from collections import OrderedDict
-
 n = int(input())
-wordlist = []
-for i in range(0,n):
-  wordlist.append(input())
-  
-duplicated = wordlist
+duplicated = []
 
-print(len(set(wordlist)))
-wordlist = list(OrderedDict.fromkeys(wordlist))
+for i in range(0,n): duplicated.append(input())
+  
+wordlist = list(OrderedDict.fromkeys(duplicated))
+print(len(wordlist))
 
 for word in wordlist:
   print(duplicated.count(word),end= " ")
